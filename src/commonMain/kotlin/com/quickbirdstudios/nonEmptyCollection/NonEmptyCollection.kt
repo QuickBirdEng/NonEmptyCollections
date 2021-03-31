90f4c5e2f8c003557fa4d741bd0bcd649c981d36
Balazs Toth <balazs.toth@quickbirdstudios.com>
2021-03-31 20:57:34 +0200

    mark generic of `NonEmptyCollection` with `out` to align it with `Collection`

--- src/commonMain/kotlin/com/quickbirdstudios/nonEmptyCollection/NonEmptyCollection.kt
+++ src/commonMain/kotlin/com/quickbirdstudios/nonEmptyCollection/NonEmptyCollection.kt
@@ -1,6 +1,6 @@
 package com.quickbirdstudios.nonEmptyCollection
 
-interface NonEmptyCollection<T> : Collection<T> {
+interface NonEmptyCollection<out T> : Collection<T> {
 
     @Deprecated(
         level = DeprecationLevel.ERROR,
@@ -8,11 +8,11 @@ interface NonEmptyCollection<T> : Collection<T> {
         replaceWith = ReplaceWith("first()")
     )
     fun firstOrNull(): T = first()
-
-    @Deprecated(
-        level = DeprecationLevel.ERROR,
-        message = "Alternative is never used!",
-        replaceWith = ReplaceWith("first()")
-    )
-    fun firstOr(alternative: () -> T): T = first()
 }
+
+@Deprecated(
+    level = DeprecationLevel.ERROR,
+    message = "Alternative is never used!",
+    replaceWith = ReplaceWith("first()")
+)
+fun <T> NonEmptyCollection<T>.firstOr(alternative: () -> T): T = first()
