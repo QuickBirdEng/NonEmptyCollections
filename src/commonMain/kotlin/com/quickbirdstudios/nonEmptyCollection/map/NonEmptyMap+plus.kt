f4fee04019266b788c966d089c5177bca521a383
Rob Agnese <ragnese@protonmail.com>
2021-03-30 20:53:39 -0400

    More efficient implementations of NonEmptySet and NonEmptyMap, and the plus operators.

--- src/commonMain/kotlin/com/quickbirdstudios/nonEmptyCollection/map/NonEmptyMap+plus.kt
+++ src/commonMain/kotlin/com/quickbirdstudios/nonEmptyCollection/map/NonEmptyMap+plus.kt
@@ -1,34 +1,29 @@
 package com.quickbirdstudios.nonEmptyCollection.map
 
-operator fun <K, V> NonEmptyMap<K, V>.plus(entry: Pair<K, V>) = copy(rest = rest + entry)
+import kotlin.collections.plus as stdPlus
 
-operator fun <K, V> Map<K, V>.plus(entry: Pair<K, V>) = run {
-    val first = entries.first()
+operator fun <K, V> NonEmptyMap<K, V>.plus(entry: Pair<K, V>): NonEmptyMap<K, V> = full + entry
 
-    NonEmptyMap(
-        first = first.key to first.value,
-        rest = this + mapOf(entry) - first.key
-    )
-}
+operator fun <K, V> Map<K, V>.plus(entry: Pair<K, V>): NonEmptyMap<K, V> = NonEmptyMap(this.stdPlus(entry))
 
-operator fun <K, V> NonEmptyMap<K, V>.plus(other: Map<K, V>) = copy(rest = rest + other)
+operator fun <K, V> NonEmptyMap<K, V>.plus(other: Map<K, V>): NonEmptyMap<K, V> = NonEmptyMap(full.stdPlus(other))
 
 operator fun <K, V> Map<K, V>.plus(
     other: NonEmptyMap<K, V>
-): NonEmptyMap<K, V> = other + this
+): NonEmptyMap<K, V> = NonEmptyMap(this.stdPlus(other.full))
 
 operator fun <K, V> NonEmptyMap<K, V>.plus(
     other: NonEmptyMap<K, V>
-): NonEmptyMap<K, V> = this as Map<K, V> + other
+): NonEmptyMap<K, V> = full + other
 
 operator fun <K, V> NonEmptyMap<K, V>.plus(
     other: Iterable<Pair<K, V>>
-): NonEmptyMap<K, V> = this + other.toMap()
+): NonEmptyMap<K, V> = NonEmptyMap(full.stdPlus(other))
 
 operator fun <K, V> NonEmptyMap<K, V>.plus(
     other: Sequence<Pair<K, V>>
-): NonEmptyMap<K, V> = this + other.toMap()
+): NonEmptyMap<K, V> = NonEmptyMap(full.stdPlus(other))
 
 operator fun <K, V> NonEmptyMap<K, V>.plus(
     other: Array<Pair<K, V>>
-): NonEmptyMap<K, V> = this + other.toMap()
+): NonEmptyMap<K, V> = NonEmptyMap(full.stdPlus(other))
